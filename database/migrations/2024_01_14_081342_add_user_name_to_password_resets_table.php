d7c69714ebc0b057ad5bc1f451e488c01c4dc25f
SungHak <sunghak@musinsa.com>
2024-01-29 00:03:59 +0900

    `password_resets` 테이블 생성을 위한 마이그레이션 파일 추가

---
+++ database/migrations/2024_01_14_081342_add_user_name_to_password_resets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            $table->string('user_id', 50)->comment('유저아이디')->after('email');
+            $table->string('user_name', 50)->default('')->comment('회원 이름')->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('password_resets', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+            $table->dropColumn('user_name');
+        });
+    }
+};
