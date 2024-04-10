b0e80a520f610f030424625cd63c7f80ed326b1b
SungHak <sunghak@musinsa.com>
2024-04-10 20:36:17 +0900

    `mm_stories`, `mm_visit_comments`  테이블 생성 마이그레이션 파일 생성

---
+++ database/migrations/2024_03_02_165029_create_mm_stories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mm_stories', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->bigIncrements('id')->unique();
+            $table->unsignedBigInteger('user_id')->default(0)->comment('회원 고유키');
+            $table->unsignedBigInteger('memorial_id')->default(0)->comment('기념관 고유키');
+            $table->text('message')->nullable()->comment('메세지');
+            $table->unsignedBigInteger('attachment_id')->nullable()->comment('첨부 파일 고유키');
+            $table->unsignedTinyInteger('is_visible')->default(1)->comment('노출 여부(0:false, 1:true)');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('mm_users')->cascadeOnDelete();
+            $table->foreign('memorial_id')->references('id')->on('mm_memorials')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mm_stories');
+    }
+};
