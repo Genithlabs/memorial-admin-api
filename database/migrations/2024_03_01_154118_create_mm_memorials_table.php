a3c9b1584c7f91a4afad8f624a36f93d03716328
SungHak <sunghak@musinsa.com>
2024-04-13 22:41:30 +0900

    `mm_memorials` 테이블에서 `user_id` 를 `integer` 로 변경하고, `is_open` 기본값을 1 로 수정함

--- database/migrations/2024_03_01_154118_create_mm_memorials_table.php
+++ database/migrations/2024_03_01_154118_create_mm_memorials_table.php
@@ -17,14 +17,14 @@ return new class extends Migration
             $table->engine = 'InnoDB';
             $table->charset = 'utf8mb4';
             $table->bigIncrements('id')->unique();
-            $table->string('user_id', 50)->unique()->comment('유저아이디')->unique();
+            $table->unsignedBigInteger('user_id')->default(0)->comment('회원 고유키');
             $table->string('name', 50)->comment('기념인 이름');
             $table->date('birth_start')->nullable()->comment('태어난 일자');
             $table->date('birth_end')->nullable()->comment('마감한 일자');
             $table->text('career_contents')->nullable()->comment('생애');
             $table->unsignedInteger('profile_attachment_id')->nullable()->comment('프로필 사진 첨부 파일 고유키');
             $table->unsignedInteger('bgm_attachment_id')->nullable()->comment('배경음악 첨부 파일 고유키');
-            $table->unsignedTinyInteger('is_open')->default(0)->comment('오픈 여부(0:false, 1:true)');
+            $table->unsignedTinyInteger('is_open')->default(1)->comment('오픈 여부(0:false, 1:true)');
             $table->timestamps();
         });
     }
