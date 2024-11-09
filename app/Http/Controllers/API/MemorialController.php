2ebd468b12551dadfe89971fd4fc71ee5420f9d4
SungHak <sunghak@musinsa.com>
2024-11-09 19:30:04 +0900

    `MemorialController` 에서 기념관 등록할 때 프로필과 BGM 파일을 `Cloudinary` 를 이용하여 업로드 하도록 수정함

--- app/Http/Controllers/API/MemorialController.php
+++ app/Http/Controllers/API/MemorialController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Attachment;
 use App\Models\Memorial;
 use App\Models\VisitorComment;
+use Cloudinary\Cloudinary;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -22,11 +23,15 @@ class MemorialController extends Controller
     private $S3_PATH_CAREER_CONTENT_FILE;
     private $S3_URL;
 
-    public function __construct() {
+    protected $cloudinary;
+
+    public function __construct(Cloudinary $cloudinary) {
         $this->S3_PATH_PROFILE = "/memorial/profile/";
         $this->S3_PATH_BGM = "/memorial/bgm/";
         $this->S3_PATH_CAREER_CONTENT_FILE = "/memorial/career/";
         $this->S3_URL = "https://foot-print-resources.s3.ap-northeast-2.amazonaws.com";
+
+        $this->cloudinary = $cloudinary;
     }
 
     public function register(Request $request) {
@@ -42,17 +47,17 @@ class MemorialController extends Controller
         $validator = Validator::make($request->all(), [
             'user_name' => 'required|max:50',
             'birth_start' => 'required|sometimes|date_format:Y-m-d',
-            'profile' => 'required|mimes:jpeg,jpg,png|max:1024',
-            'bgm' => 'sometimes|mimes:mp3,mp4,mpa,m4a|max:4096',
+            'profile' => 'required|mimes:jpeg,jpg,png|max:10240',
+            'bgm' => 'sometimes|mimes:mp3,mp4,mpa,m4a|max:102400',
         ], [
             'user_name.required' => '기념인 이름을 입력해 주세요',
             'user_name.max' => '기념인 이름은 50자 이내로 입력해 주세요',
             'birth_start.required' => '기념인 태어난 생년월일을 입력해 주세요',
             'profile.required' => '기념인 프로필 사진을 선택해 주세요',
             'profile.mimes' => '기념인 프로필 사진은 jpg/jpeg/png 형식이여야 합니다',
-            'profile.max' => '기념인 프로필 사진은 1Mb 이하여야 합니다',
+            'profile.max' => '기념인 프로필 사진은 10Mb 이하여야 합니다',
             'bgm.mimes' => '기념관 배경 음악은 mp3, mp4, mpa, m4a 형식이여야 합니다',
-            'bgm.max' => '기념관 배경 음악은 4Mb 이하여야 합니다'
+            'bgm.max' => '기념관 배경 음악은 100Mb 이하여야 합니다'
         ]);
 
         if ($validator->fails()) {
@@ -84,17 +89,23 @@ class MemorialController extends Controller
             $file = $request->file('profile')->getClientOriginalName();
             $extension = pathinfo($file, PATHINFO_EXTENSION);
             $lowerExtentsion = strtolower($extension);
-            $fileName = $memorial->id."_profile.".$lowerExtentsion;
-            $profilePathFileName = $this->S3_PATH_PROFILE.$fileName;
+            $fileName = $memorial->id."_profile";
 
-            $exists = Storage::disk('s3')->exists($profilePathFileName);
-            if ($exists) {
-                Storage::disk('s3')->delete($profilePathFileName);
-            }
-            Storage::disk('s3')->put($profilePathFileName, file_get_contents($profile_url));
+            $profileUploadResponse = $this->cloudinary->uploadApi()->upload($profile_url->getRealPath(), $options = [
+                'public_id' => $fileName,
+                'asset_folder' => $this->S3_PATH_PROFILE,
+                'resource_type' => "image",
+                'use_filename' => true, // 원본 파일명을 public_id 로 사용함
+            ]);
+
+            $publicId = $profileUploadResponse['public_id'];
+            $version = $profileUploadResponse['version'];
+
+            $filePath = "/".env('CLOUDINARY_NAME')."/image/upload/v".$version."/";
+            $fileName = $publicId.".".$lowerExtentsion;
 
             $profileAttachment = new Attachment();
-            $profileAttachment->file_path = $this->S3_PATH_PROFILE;
+            $profileAttachment->file_path = $filePath;
             $profileAttachment->file_name = $fileName;
             $profileAttachment->save();
 
@@ -107,17 +118,23 @@ class MemorialController extends Controller
                 $file = $request->file('bgm')->getClientOriginalName();
                 $extension = pathinfo($file, PATHINFO_EXTENSION);
                 $lowerExtentsion = strtolower($extension);
-                $fileName = $memorial->id."_bgm.".$lowerExtentsion;
-                $bgmPathFileName = $this->S3_PATH_BGM.$fileName;
+                $fileName = $memorial->id."_bgm";
 
-                $exists = Storage::disk('s3')->exists($bgmPathFileName);
-                if ($exists) {
-                    Storage::disk('s3')->delete($bgmPathFileName);
-                }
-                Storage::disk('s3')->put($bgmPathFileName, file_get_contents($bgm_url));
+                $bgmUploadResponse = $this->cloudinary->uploadApi()->upload($bgm_url->getRealPath(), $options = [
+                    'public_id' => $fileName,
+                    'asset_folder' => $this->S3_PATH_BGM,
+                    'resource_type' => "video",
+                    'use_filename' => true, // 원본 파일명을 public_id 로 사용함
+                ]);
+
+                $publicId = $bgmUploadResponse['public_id'];
+                $version = $bgmUploadResponse['version'];
+
+                $filePath = "/".env('CLOUDINARY_NAME')."/video/upload/v".$version."/";
+                $fileName = $publicId.".".$lowerExtentsion;
 
                 $bgmAttachment = new Attachment();
-                $bgmAttachment->file_path = $this->S3_PATH_BGM;
+                $bgmAttachment->file_path = $filePath;
                 $bgmAttachment->file_name = $fileName;
                 $bgmAttachment->save();
 
